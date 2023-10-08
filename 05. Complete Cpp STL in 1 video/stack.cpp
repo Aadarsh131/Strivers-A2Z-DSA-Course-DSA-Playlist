1bffd7bc387f1b498fa66d101e45ff365981b8de
Aadarsh131 <aadarshkumar131@gmail.com>
2023-10-08 14:24:07 +0530

    changes

--- 05. Complete Cpp STL in 1 video/stack.cpp
+++ 05. Complete Cpp STL in 1 video/stack.cpp
@@ -2,17 +2,29 @@
 #include <stack>
 using namespace std;
 
-void loopOver(stack<int> a)
+void loopOver(stack<int> &a)
 {
     if (a.empty())
         return;
-
-    cout << a.top() << " ";
+    int x = a.top();
+    cout << x << " ";
     a.pop();
     loopOver(a);
-    a.push(a.top());
+    a.push(x);
+    // a.push(x.top())//this would give the segmentation fault execption, hence we are storing the value in x
 }
 
+// template <typename T>
+// void traverseStack(std::stack<T> &stk) {
+//     if (stk.empty()) {
+//         return;
+//     }
+//     T x = stk.top();
+//     std::cout << x << " ";
+//     stk.pop();
+//     traverseStack(stk);
+//     stk.push(x);
+// }
 int main()
 {
     stack<int> a;
@@ -53,6 +65,23 @@ int main()
 
     // #2 way(using recursion, heap mem is used)-
     loopOver(a);
+    // traverseStack(a);
     cout << endl;
-    
+
+    // Both of the above way are ineffective, instead use deque
+    // #3 way(deque- it can be iterated over range-based for-loops)-
+    deque<int> d;
+    d.push_front(1); // use push_front() and pop_front() only to simulate the stack
+    d.push_front(2);
+    d.push_front(3);
+    d.push_front(4);
+
+    d.pop_front(); // will pop out the last inserted push_front() element
+
+    // d.push_back(0); // this operation doesn't replicated the stack way, infact it breaks the rule of LIFO, hence, dont use it, while using deque as stack, push_front() and pop_front() follows the LIFO exactly
+    for (auto i : d)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
 }
